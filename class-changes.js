1eabefe98d92d9c5bb5605e65d2abce9299a9a73
kaihanz1 <kzho0028@student.monash.edu>
2025-05-29 16:19:12 +1000

    upload first time

---
+++ class-changes.js
@@ -0,0 +1,22 @@
+const classChanges = [
+  { time: 6, target: ".navtitle1", add: "active" },
+  { time: 7, target: ".navtitle1", remove: "active" },
+  { time: 9, target: ".changeme", remove: "show" },
+  { time: 9, target: ".changeme", add: "hidden" },
+
+  { time: 9, target: ".highlight", add: "active" },
+  { time: 11, target: ".highlight", remove: "active" },
+
+  { time: 5, target: ".pop1", add: "active" },
+  { time: 5, target: ".pop1", add: "remove" },
+
+  { time: 9, target: ".changeme2", add: "show" },
+  { time: 16, target: ".changeme2", remove: "show" },
+  { time: 16, target: ".changeme2", add: "hidden" },
+
+  { time: 16, target: ".changeme3", add: "show" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
+
+
