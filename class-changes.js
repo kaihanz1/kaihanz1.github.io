5aaaa79dda969b4ee9b38811aafbbd7f489ba3ad
kaihanz1 <kzho0028@student.monash.edu>
2025-06-06 12:05:03 +1000

    Bones Friday

--- class-changes.js
+++ class-changes.js
@@ -4,10 +4,57 @@ const classChanges = [
   { time: 9, target: ".changeme", remove: "show" },
   { time: 9, target: ".changeme", add: "hidden" },
 
-  { time: 9, target: ".highlight", add: "active" },
-  { time: 11, target: ".highlight", remove: "active" },
 
-  { time: 5, target: ".pop1", add: "active" },
+// SCRIPT HIGHLIGHTS 
+
+  { time: 34, target: ".p1", add: "highlight" },
+  { time: 97, target: ".p1", remove: "highlight" },
+
+  { time: 98, target: ".p2", add: "highlight" },
+  { time: 164, target: ".p2", remove: "highlight" },
+
+  { time: 164, target: ".p3", add: "highlight" },
+  { time: 185, target: ".p3", remove: "highlight" },
+
+  { time: 186, target: ".p4", add: "highlight" },
+  { time: 210, target: ".p4", remove: "highlight" },
+
+  { time: 211, target: ".p5", add: "highlight" },
+  { time: 248, target: ".p5", remove: "highlight" },
+
+  { time: 249, target: ".p6", add: "highlight" },
+  { time: 267, target: ".p6", remove: "highlight" },
+
+  { time: 267, target: ".p7", add: "highlight" },
+  { time: 288, target: ".p7", remove: "highlight" },
+
+  { time: 288, target: ".p8", add: "highlight" },
+  { time: 331, target: ".p8", remove: "highlight" },
+
+  { time: 331, target: ".p9", add: "highlight" },
+  { time: 341, target: ".p9", remove: "highlight" },
+
+  { time: 341, target: ".p10", add: "highlight" },
+  { time: 373, target: ".p10", remove: "highlight" },
+
+  { time: 374, target: ".p11", add: "highlight" },
+  { time: 401, target: ".p11", remove: "highlight" },
+
+  { time: 401, target: ".p12", add: "highlight" },
+  { time: 426, target: ".p12", remove: "highlight" },
+
+  { time: 426, target: ".p13", add: "highlight" },
+  { time: 465, target: ".p13", remove: "highlight" },
+
+
+
+  // { time: 99, target: ".p5", add: "highlight" },
+  // { time: 163, target: ".p5", remove: "highlight" },
+
+  // { time: 164, target: ".p6", add: "highlight" },
+  // { time: 184, target: ".p6", remove: "highlight" },
+
+  { time: 185, target: ".pop1", add: "active" },
   { time: 5, target: ".pop1", add: "remove" },
 
   { time: 9, target: ".changeme2", add: "show" },
@@ -18,5 +65,7 @@ const classChanges = [
   { time: 28, target: ".changeme3", remove: "show" },
   { time: 28, target: ".changeme3", add: "hidden" }
 ];
+
+
 
 
